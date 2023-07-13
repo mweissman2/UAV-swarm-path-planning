7e8d22626c828f4a8912416146050e046823dfd4
CuriousBeluga <seantseng4589@gmail.com>
2023-07-12 20:18:47 -0400

    Added new reward tiers to MADDPG agent reward function

--- maddpg_code.py
+++ maddpg_code.py
@@ -11,11 +11,18 @@ AGENT_RADIUS = 10  # Radius of the agent
 OBSTACLE_RADIUS = 30  # Radius of the obstacles
 MOVEMENT_SPEED = 3  # Movement speed of the agent
 
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 # class for making individual agents
 class MADDPG_agent:
     def __init__(self, initial, goal, temp, e_th, obstacles, ID):
         self.position = initial  # Current position of the agent, 1x2 list
+        self.start = initial
         self.goal = goal  # Target position to reach, 1x2 list
         self.path = []  # initialize short term memory
         self.path_length = 0
@@ -23,12 +30,36 @@ class MADDPG_agent:
         self.agent_id = ID
         self.next_reward_neighbor = []
         self.reward_mem = []
+        self.disp_path = []
         self.long_mem = []  # long term memory path
 
         # algorithm parameters
         self.temp = temp
         self.e_th = e_th
 
+    def move(self):
+        if self.long_mem:
+            next_pos = self.long_mem[0]
+            dx = next_pos[0] - self.position[0]
+            dy = next_pos[1] - self.position[1]
+            distance = (dx ** 2 + dy ** 2) ** 0.5
+            self.position = (next_pos[0], next_pos[1])
+            self.long_mem.pop(0)
+
+            # if distance <= MOVEMENT_SPEED:
+
+            # else:
+            #     direction_x = int(dx / distance * MOVEMENT_SPEED)
+            #     direction_y = int(dy / distance * MOVEMENT_SPEED)
+            #     self.position = (self.position[0] + direction_x, self.position[1] + direction_y)
+
+            # print("dist: " + str(distance))
+            # print("position: " + str(self.position))
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, GREEN, self.position, AGENT_RADIUS)
+
+    # Checking if available neighbors are near the simulation boundary or obstacle
     def is_valid(self, node):
         x, y = node
         if x < 0 or x >= WIDTH or y < 0 or y >= HEIGHT:
@@ -57,13 +88,6 @@ class MADDPG_agent:
         self.temp = temp_update
         self.e_th = e_update
 
-    # def count_critic(self):  # agent count down critic
-    #     countdown = self.count
-    #     countdown -= 1
-    #     if countdown == 0:  # maybe make this some other variable instead of bool
-    #         return False
-    #     if countdown > 0:
-    #         return True
 
     def get_param(self):
         return self.e_th, self.temp
@@ -77,11 +101,21 @@ class MADDPG_agent:
         reward = 0
         for obstacle in self.obstacles:
             p0 = AGENT_RADIUS + obstacle.radius  # Influence radius of F_rep
+            p1 = p0 + 3
             distance = self.euclid_distance((x, y), (obstacle.x, obstacle.y))
-            if distance < p0:
+
+            # Setting reward to 0 for collision case (perhaps change to a negative value later
+            if distance <= p0:
                 reward = 0
+            # set negative reward for being close to the obstacle
+            elif p0 < distance < p1:
+                reward = -(max_reward - (abs(x1 - x2) + abs(y1 - y2)) ** 0.5)/2
+
             else:
                 reward = max_reward - (abs(x1 - x2) + abs(y1 - y2)) ** 0.5
+
+        # need to add some reward to allow the agents to freely backtrack if stuck
+        # this could mean adding another formula to incentivize constant movement
 
         return reward
 
@@ -106,39 +140,40 @@ class MADDPG_agent:
             delta = current_energy - next_energy  # calculating energy cost delta for criterion calculation
 
             # Debugging/Tuning print statements
-            print("temp: " + str(self.temp))
-            print("delta: " + str(delta))
-            print("pos 1: " + str(self.position) + " pos 2: " + str(neighbor))
-            print("curr reward: " + str(current_energy))
-            print("next reward: " + str(next_energy))
+            # print("temp: " + str(self.temp))
+            # print("delta: " + str(delta))
+            # print("pos 1: " + str(self.position) + " pos 2: " + str(neighbor))
+            # print("curr reward: " + str(current_energy))
+            # print("next reward: " + str(next_energy))
 
             # Occasionally the probability calc is not a real number result, if so probability is set to 0
             try:
                 prob = math.exp(-delta / self.temp)
             except:
-                print("Didn't Calculate")
+                # print("Didn't Calculate")
                 prob = 0
 
             if delta < 0:
-                print("accepted best option")
+                # print("accepted best option")
                 self.position = neighbor
 
             elif prob > self.e_th:
                 self.position = neighbor
 
                 # Debugging/Tuning print statements
-                print("accepted possibly worse option")
-                print("prob: " + str(prob))
-                print("eth: " + str(self.e_th))
+                # print("accepted possibly worse option")
+                # print("prob: " + str(prob))
+                # print("eth: " + str(self.e_th))
 
-            else:
+            # else:
                 # Debugging/Tuning print statements
-                print("stayed")
-                print("prob: " + str(prob))
-                print("eth: " + str(self.e_th))
+                # print("stayed")
+                # print("prob: " + str(prob))
+                # print("eth: " + str(self.e_th))
 
             sa_path.append(self.position)
             self.long_mem.append(self.position)
+            self.disp_path.append(self.position)
 
         self.next_reward_neighbor = self.position
         return sa_path, self.path_length, self.reward(self.position[0], self.position[1])  # return path route and distance to the target, euclid distance
@@ -165,86 +200,3 @@ class MADDPG_agent:
 
         return max(reward_1, reward_2, reward_3, reward_4)
 
-    # # Below would be implemented in the MultiAgentEnvironment.py file under function mad_search
-# # ------------------------------------------------------------------------------------------------------------------
-#
-# N = 100                     # number of episodes
-# y = .45                     # reward discount, basically minimizes the max reward in the Q equation
-# q_1 = []                    # list of q-values
-# q_2 = []
-#
-# path1_store = []            # storing the total path
-# path2_store = []
-#
-# e_th = .56
-# cool = 0.05
-# temp = 1500
-# count = 50
-#
-# initial = [5,4]
-# final = [14,6]
-#
-# # create agents
-#
-# agent1 = MADDPG(initial,final,e_th,cool,temp,count)  # argument order is currently incorrect
-# agent2 = MADDPG(initial,final,e_th,cool,temp,count)
-#
-# for episode in range(1, N):
-#
-#
-#     #command agents to commit actions, 5 transitions/ actions per episode = receive 5 transitions/actions as a result
-#     path1, length1 = agent1.action()
-#     path2, length2 = agent2.action()
-#
-#     path1_store += path1
-#     path2_store += path2
-#
-#     #compute the value of each Q-value
-#     #one for simulated annealing
-#     # formula is Q = R + y*max(reward)
-#     # R is calculating immediate reward of an action, so reward function calls one of the action algos
-#     #the sampling of different actions will be thru using different parameters of the SA function
-#     #y is the discount rate, which is multiplied by the maximum reward
-#     q_1[episode] = agent1.reward(path1[-1],path1[-2])   #the reward, argument will be current position of agent at end of the action
-#     q_2[episode] = agent2.reward(path2[-1], path2[-2])
-#     #this q-value will determine the critic update
-#     # which modifies the threshold  param of the SA algo
-#
-#     #sample a previous slope
-#         #if new slope is greater, q values are rising and temperature can cool even more --> higher cooling rate, higher threshold
-#     gradient_1 = (q_1[episode-2]-q_1[episode-1])/(2)       #simple rise over run calculation
-#     gradient_2 = (q_2[episode-2]-q_2[episode-1])/(2)
-#
-#     new_gradient1 = (q_1[episode-1]-q_1[episode])/(2)
-#     new_gradient2 = (q_2[episode-1]-q_2[episode])/(2)
-#
-#     e_th_1, temp_1, cool_rate_1 = agent1.get_param()
-#     e_th_2, temp_2, cool_rate_2 = agent2.get_param()
-#
-#     if new_gradient1 < gradient_1:
-#         e_update_1 = e_th_1 - .03
-#         cool_update_1 = cool_rate_1 - .1
-#         temp_update_1 = temp_1 - 100
-#         # add count update too?
-#
-#     if new_gradient1 > gradient_1:
-#         e_update_1 = e_th_1 + .03
-#         cool_update_1 = cool_rate_1 + .1
-#         temp_update_1 = temp_1 + 100
-#
-#     if new_gradient2 < gradient_2:
-#         e_update_2 = e_th_2-.03
-#         cool_update_2 = cool_rate_2-.1
-#         temp_update_2 = temp_2 -100
-#         # add count update too?
-#
-#     if new_gradient2 > gradient_2:
-#         e_update_2 = e_th_2 + .03
-#         cool_update_2 = cool_rate_2 + .1
-#         temp_update_2 = temp_2 + 100
-#         # add count update too?
-#
-#
-#     # update critic according to conditions
-#     agent1.update_critic(e_update_1,cool_update_1, temp_update_1)
-#     agent2.update_critic(e_update_2,cool_update_2, temp_update_2)
